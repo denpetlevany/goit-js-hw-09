ee8e06e3eefe23fe694e1d8c2d53b9cad58cd4d5
denpetlevany <95420949+denpetlevany@users.noreply.github.com>
2022-11-09 07:36:18 +0000

    Deploying to gh-pages from @ denpetlevany/goit-js-hw-09@94a76298918843e55a1a19eca90801de859c9f29 🚀

---
+++ 01-color-switcher.a84891cc.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled","disabled"),e.removeAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled","disabled"),e.setAttribute("disabled","disabled")})),e.setAttribute("disabled","disabled");let d=null;
+//# sourceMappingURL=01-color-switcher.a84891cc.js.map
